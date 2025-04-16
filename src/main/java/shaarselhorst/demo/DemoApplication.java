75f5fd3619aff593243846207793d044a624cbae
Stan HvdG <shaarselhorst@ilionx.com>
2025-04-16 16:15:35 +0200

    Standard response

--- src/main/java/shaarselhorst/demo/DemoApplication.java
+++ src/main/java/shaarselhorst/demo/DemoApplication.java
@@ -2,12 +2,33 @@ package shaarselhorst.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	@RequestMapping("/")
+	@ResponseBody
+	public Message displayMessage() {
+		return new Message();
+	}
+
+	static class Message {
+		private String content = "Greetings!";
+
+		public String getContent() {
+			return content;
+		}
+
+		public void setContent(String content) {
+			this.content = content;
+		}
+	}
 }
